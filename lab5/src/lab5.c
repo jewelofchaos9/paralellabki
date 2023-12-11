09544bc75d24325c3a7b3d4f361ad2347b85ec73
Степан Никифоров <sanikiforov@hex.team>
2023-12-11 20:53:58 +0000

    kek

---
+++ lab5/src/lab5.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <mpi.h>
+#include <omp.h>
+
+int main(int argc, char** argv)
+{
+	int ret  = -1;	///< For return values
+	int size = -1;	///< Total number of processors
+	int rank = -1;	///< This processor's number
+
+	const int count = 1000000; ///< Number of array elements
+
+	int* array = 0; ///< The array we need to find the max in
+	int lmax = -1;	///< Local maximums
+	int  max = -1;  ///< The maximal element
+  double start, end;
+
+  const int random_seed = atoi(argv[1]); ///< RNG seed
+	/* Initialize the MPI */
+	ret = MPI_Init(&argc, &argv);
+
+	/* Determine our rank and processor count */
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	/* Allocate the array */
+  array = (int*)malloc(count * sizeof(int));
+  if(!rank){
+    srand(random_seed);
+    for (int i = 0; i < count; i++) { array[i] = rand(); }
+  }
+    
+  start = MPI_Wtime();
+	MPI_Bcast(array, count, MPI_INTEGER, 0, MPI_COMM_WORLD);
+
+	const int wstart = (rank    ) * count / size;
+	const int wend   = (rank + 1) * count / size;
+
+	for (int i = wstart; i < wend; i++){
+		if (array[i] > lmax){ 
+            lmax = array[i]; 
+      }
+	}
+
+	MPI_Reduce(&lmax, &max, 1, MPI_INTEGER, MPI_MAX, 0, MPI_COMM_WORLD);
+	ret = MPI_Finalize();
+  end = MPI_Wtime();
+  if (!rank) {
+    printf("%g", end-start);
+  }
+	return(0);
+}
---
+++ lab5/src/parallel_core.py
@@ -0,0 +1,172 @@
+from random import randrange
+import subprocess
+from logger import logger
+import matplotlib.pyplot as plt
+from abc import ABC
+
+AVG_RUNS = 20
+MAX_SYSTEM_THREADS = 8
+MAX_TEST_THREAD = 30
+
+
+class ParallelExecutor:
+    def __init__(self, program_name: str) -> None:
+        self.program = program_name
+
+    def __average_for_n_threads(self, threads_num: int) -> float:
+        timings = []
+        for _ in range(AVG_RUNS):
+            seed = randrange(0, 2**16)
+            p = subprocess.run([self.program, str(threads_num), str(seed)], stdout=subprocess.PIPE)
+            out = p.stdout
+            timings.append(float(out))
+            logger.info(f"subprocess with {threads_num} threads finished with timing {out}")
+
+        return sum(timings) / len(timings)
+
+    def __average_for_n_threads_mpi(self, threads_num: int) -> float:
+        timings = []
+        for _ in range(AVG_RUNS):
+            seed = randrange(0, 2**16)
+            p = subprocess.run(['mpicc', '-c', str(threads_num), self.program,  str(seed)], stdout=subprocess.PIPE)
+            out = p.stdout
+            timings.append(float(out))
+            logger.info(f"subprocess with mpi {threads_num} threads finished with timing {out}")
+
+        return sum(timings) / len(timings)
+
+    def get_graph_experemental_info(self, mpi=False):
+        func = self.__average_for_n_threads_mpi if mpi else self.__average_for_n_threads
+        graph_data = []
+        for thread_num in range(1, MAX_TEST_THREAD):
+            point = [thread_num, func(thread_num)]
+            graph_data.append(point)
+
+        return graph_data
+
+
+class ABCDataCore(ABC):
+    def __init__(self, plot_data):
+        self.experimental_data = plot_data
+
+    def theoretical_average_time(self, x: int):
+        pass
+
+    def theoretical_efficiency(self, x: int):
+        pass
+
+    def theoretical_acceleration(self, x: int):
+        pass
+
+    def load_theoretical_average_time(self):
+        x = [i[0] for i in self.experimental_data]
+        y = [self.theoretical_average_time(i) for i in x]
+        return x, y
+
+    def load_theoretical_efficiency(self):
+        x = [i[0] for i in self.experimental_data]
+        y = [self.theoretical_efficiency(i) for i in x]
+        return x, y
+
+    def load_theoretical_acceleration(self):
+        x = [i[0] for i in self.experimental_data]
+        y = [self.theoretical_acceleration(i) for i in x]
+        return x, y
+
+    def load_practical_average_time(self):
+        pass
+
+    def load_practical_efficiency(self):
+        pass
+
+    def load_practical_acceleration(self):
+        pass
+
+
+class Lab1DataCore(ABCDataCore):
+    def theoretical_average_time(self, x):
+        base_point = self.experimental_data[0][1]
+        if x < MAX_SYSTEM_THREADS:
+            return base_point/x
+        return base_point / MAX_SYSTEM_THREADS
+
+    def theoretical_efficiency(self, x):
+        if x <= MAX_SYSTEM_THREADS:
+            return 1
+        return 1/(x - MAX_SYSTEM_THREADS)
+
+    def theoretical_acceleration(self, x):
+        if x <= MAX_SYSTEM_THREADS:
+            return x 
+        return MAX_SYSTEM_THREADS
+
+    def load_practical_average_time(self):
+        x = [i[0] for i in self.experimental_data]
+        y = [i[1] for i in self.experimental_data]
+        return x, y
+
+    def load_practical_efficiency(self):
+        base_point = self.experimental_data[0][1]
+        x = [i[0] for i in self.experimental_data]
+        y = [base_point/(i[1]*i[0])for i in self.experimental_data]
+        return x, y
+
+    def load_practical_acceleration(self):
+        base_point = self.experimental_data[0][1]
+        x = [i[0] for i in self.experimental_data]
+        y = [base_point/i[1] for i in self.experimental_data]
+        return x, y
+
+
+class Visualizer:
+    def __init__(self, binary_name: str, binary_name2: str):
+        self.executor = ParallelExecutor(binary_name)
+        self.executor2 = ParallelExecutor(binary_name2)
+        self.experimental_data2 = self.executor2.get_graph_experemental_info(mpi=True)
+        self.experimental_data = self.executor.get_graph_experemental_info()
+        self.data_core = Lab1DataCore(self.experimental_data)
+        self.data_core2 = Lab1DataCore(self.experimental_data2)
+
+    def visualize_average_time(self, name="AVG_time.png"):
+        xt, yt = self.data_core.load_theoretical_average_time()
+        xp, yp = self.data_core2.load_theoretical_average_time()
+        fig, ax = plt.subplots(layout='constrained')
+        ax.plot(xt, yt, label="MPI AVG Time")
+        ax.plot(xp, yp, label="OpenMP AVG Time")
+        ax.set_xlabel("Threads")
+        ax.set_ylabel("Time")
+        ax.legend()
+        fig.savefig(name)
+
+    def visualize_acceleration(self, name="acceleration.png"):
+        xt, yt = self.data_core.load_practical_acceleration()
+        xp, yp = self.data_core2.load_practical_acceleration()
+        fig, ax = plt.subplots(layout='constrained')
+        ax.plot(xt, yt, label="MPI Acceleration")
+        ax.plot(xp, yp, label="OpenMP Acceleration")
+        ax.set_xlabel("Threads")
+        ax.set_ylabel("Acceleration")
+        ax.legend()
+        fig.savefig(name)
+
+    def visualize_efficiency(self, name="efficiency.png"):
+        xt, yt = self.data_core.load_theoretical_efficiency()
+        xp, yp = self.data_core2.load_theoretical_efficiency()
+        fig, ax = plt.subplots(layout='constrained')
+        ax.plot(xt, yt, label="MPI Efficiency")
+        ax.plot(xp, yp, label="OpenMP Efficiency")
+        ax.set_xlabel("Threads")
+        ax.set_ylabel("Efficiency")
+        ax.legend()
+        fig.savefig(name)
+
+
+def main():
+    v = Visualizer('./a.out', './b.out')
+    v.visualize_average_time()
+    v.visualize_efficiency()
+    v.visualize_acceleration()
+
+if __name__ == "__main__":
+    main()
+
